4096080ac415a4ca5241b741055b4b911db81d81
aLizlab-Nancy <nancy@alizlab.com>
2021-07-16 01:24:12 +0900

    feat(setup): tsの準備とbuildの実装

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,41 @@
 const gulp = require('gulp');
 const rename = require('gulp-rename');
+const del = require('del');
 
+// 通知関係
 const plumber = require("gulp-plumber");
 const notifier = require('node-notifier');
 
+// ブラウザ関係
 const browser = require("browser-sync");
 
+// HTML 系
 const ejs = require('gulp-ejs');
 
+// CSS 系
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
+// TODO: いつか頑張って追加する
+// const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
 
+// JS 系
+const typescript = require('gulp-typescript');
 const babel = require('gulp-babel');
 
+// 画像系
+const imagemin = require('gulp-imagemin');
+const imageminJpg = require('imagemin-jpeg-recompress');
+const imageminPng = require('imagemin-pngquant');
+const imageminGif = require('imagemin-gifsicle');
+const svgmin = require('gulp-svgmin');
+const webp = require('gulp-webp');
+
+// Build 系
+const uglify = require('gulp-uglify');
+
+
+// コンパイルエラーの時のメッセージ
 function onError (error) {
   notifier.notify(
     {
@@ -24,10 +46,6 @@ function onError (error) {
       console.log(error.message);
     }
   );
-}
-
-function taskClean (done) {
-  done();
 }
 
 // ローカルサーバを起動
@@ -66,6 +84,13 @@ function taskEjs (done) {
 // scss のビルドを行う
 function taskSass (done) {
   const plugins = [
+    // tailwindcss({
+    //   jit: true,
+    //   purge: [
+    //     './public/**/*.html',
+    //     './src/ejs/**/*.ejs',
+    //   ],
+    // }),
     autoprefixer({ cascade: false }),
   ];
 
@@ -82,14 +107,18 @@ function taskSass (done) {
   done();
 }
 
-// js のビルドを行う
-function taskJs (done) {
-  gulp.src(['./src/js/**/*.js', '!./src/js/**/_*.js'])
+// ts のビルドを行う
+function taskTs (done) {
+  gulp.src(['./src/js/**/*.ts', '!./src/js/**/_*.ts'])
     .pipe(
       plumber({
         errorHandler: onError
       })
     )
+    .pipe(typescript({
+      noImplicitAny: true,
+      removeComments: true,
+    }))
     .pipe(babel())
     .pipe(gulp.dest('./public/js'));
 
@@ -100,10 +129,80 @@ function taskJs (done) {
 function taskSurvey (done) {
   gulp.watch('./src/ejs/**/*.ejs', gulp.series(taskEjs, taskReload));
   gulp.watch('./src/scss/**/*.scss', gulp.parallel(taskSass, taskReload));
-  gulp.watch('./src/ejs/**/*.js', gulp.series(taskJs, taskReload));
+  gulp.watch('./src/ejs/**/*.ts', gulp.series(taskTs, taskReload));
 
   done();
 }
 
-exports.clean = taskClean;
-exports.default = gulp.series(gulp.parallel(taskEjs, taskSass, taskJs), taskServer, taskSurvey);
+// 不要なファイルの削除
+function taskClean (done) {
+  del(['./dist', './public/css', './public/js', './public/**/*.html']);
+
+  done();
+}
+
+// 以前の公開用のフォルダの削除
+function taskBuildDel (done) {
+  del('./dist');
+
+  done();
+}
+
+// 公開用のフォルダの生成
+function taskBuildFile (done) {
+  gulp.src(['./public/css/**/*.css', './public/**/*.html'], {
+      base: './public/'
+    })
+    .pipe(gulp.dest('./dist/'));
+
+  gulp.src('./public/js/**/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js/'));
+
+  done();
+}
+
+// 公開用の画像の圧縮
+function taskBuildImgMin (done) {
+  gulp.src('./public/img/**/*.{jpg,jpeg,png,gif}')
+    .pipe(imagemin([
+        imageminPng(),
+        imageminJpg(),
+        imageminGif({
+          interlaced: false,
+          optimizationLevel: 3,
+          colors:180
+        })
+      ]))
+    .pipe(gulp.dest('./dist/img/'));
+
+  gulp.src('./public/img/**/*.svg')
+    .pipe(svgmin())
+    .pipe(gulp.dest('./dist/img/'));
+
+  done();
+}
+
+// 公開用の Webp の生成
+function taskBuildWebp (done) {
+  gulp.src('./dist/img/**/*.{jpg,jpeg,png,gif}')
+    .pipe(webp())
+    .pipe(gulp.dest('./dist/img'));
+
+  done();
+}
+
+// Webp の生成
+function taskWebp (done) {
+  gulp.src('./public/img/**/*.{jpg,jpeg,png,gif}')
+    .pipe(webp())
+    .pipe(gulp.dest('./public/img'));
+
+  done();
+}
+
+exports.default = gulp.series(gulp.parallel(taskEjs, taskSass, taskTs), taskServer, taskSurvey);
+exports.clean = gulp.series(taskClean);
+exports.build = gulp.series(taskBuildFile, taskBuildImgMin);
+exports.webp = gulp.series(taskWebp);
+exports.webpBuild = gulp.series(taskBuildWebp);
